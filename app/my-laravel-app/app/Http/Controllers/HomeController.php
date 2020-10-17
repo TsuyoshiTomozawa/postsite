8967d585610199d293df1a59f5ab3dd04f0ccc6c
Tsuyoshi Tomozawa <sphdm244@gmail.com>
2020-10-17 11:59:09 +0900

    一覧画面に画像表示

--- app/my-laravel-app/app/Http/Controllers/HomeController.php
+++ app/my-laravel-app/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -13,8 +14,6 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        Log::debug(__METHOD__);
-
         $this->middleware('auth');
     }
 
@@ -25,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = Post::all();
+        $title = 'postsite';
+        return view('home', ["posts" => $posts, "title" => $title]);
     }
 }
